aad1075c5ad5e3068ce0fc9a0801f08925790bcb
Jayan Patel <jayanp0202@gmail.com>
2026-07-06 19:39:56 +0000

    inititate board + state variables

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
+import os
 import time
-import 
+import random as r
+import keyboard as key
+
+WIDTH, HEIGHT = 40, 20
+
+snake = [[10,15], [10,14], [10,13]]
+food = [5,20]
+direction = [0,1] #right
+
+def draw_board():
+    board = [[' ' for _ in range(WIDTH)] for _ in range(HEIGHT)]
+    # draw food
+    board[food[0]][food[1]] = 'O'
+    # draw snake
+    for i, segment in enumerate(snake):
+        char = 'X' if i == 0 else '#'
+        board[segment[0]][segment[1]] = char
+    
+    # render
+    output = "\033[H" + "\n".join("".join(row) for row in board)
+    print(output, end="", flush=True)
